d2fe608695d6e07511f49ac6a5b77c01ea45a94c
Chris Tooley <ctooley@riotgames.com>
2014-08-24 18:06:27 -0500

    modifications by Chris

--- cachematrix.R
+++ cachematrix.R
@@ -2,54 +2,50 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-  
   ## Initialize to null to reset values of matrix in the function
   my_matrix <- NULL
-  
-  
+
   set <- function(y) {
     x <<- y
     my_matrix <<- NULL
   }
-  
+
   ## Function to retrieve matrix
   get <- function() x
-  
+
   ## Function that finds the inverse of the matrix
-  setmatrix <- function(solve) 
-  my_matrix <<- solve
-  
+  setmatrix <- function(cur_matrix = matrix(x), ...) {
+    my_matrix <<- solve(cur_matrix, ...)
+  }
+
   ## Function to retrieve cached matrix
   getmatrix <- function() 
-  my_matrix
+    my_matrix
   
   
   list(set = set, get = get,
-       setmatrix = setmatrix)
+       setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
+  
   ## Return a matrix that is the inverse of 'x'
-  my_matrix <- x$getmatrix
+  matrix_inverse <- x$getmatrix()
   
   ## If the matrix is not empty, print a message and return the matrix
-  if(!is.null(my_matrix)) {
+  if(!is.null(matrix_inverse)) {
     message("Getting cached data")
-    return(my_matrix)
+    return(matrix_inverse)
   }
   
   ## Use the get function to get the cached version of the matrix
   my_matrix <- x$get()
   
   ## Use the solve function to find the inverse of the cached matrix
-  my_matrix <- setmatrix(my_matrix)
+  matrix_inverse <- setmatrix(my_matrix, ...)
   
-  ## Cache the print the inverse of the matrix
-  x$setmatrix(my_matrix)
   my_matrix
 }
